dac1cb6c5ab7ad66e58151abb7d7c0ad57b474e7
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-09-18 21:56:49 -0300

    Tela de inicialização e navegação para tela de login

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Image, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './Login';
+
+const Stack = createStackNavigator();
+
+function SplashScreen({ navigation }) {
+    useEffect(() => {
+      setTimeout(() => {
+        navigation.replace('Login');
+      }, 2000); // 2 segundos
+    }, [navigation]);
+
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./assets/logotipo_econecta_341x98.png')}
+          style={styles.image}
+          />
+        <StatusBar style="auto" />
+      </View>
+    );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+        name="Splash"
+        component={SplashScreen}
+        options={{ headerShown: false}}
+        />
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#364b56',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 280,
+    height: 70,
   },
 });
